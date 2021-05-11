386406127887a954b3b6e71aa575547073fd1727
PTSkAlman <joel.abrahamsson@elev.ga.ntig.se>
2021-05-11 11:21:36 +0200

    GUI Update

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,61 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel(new GridBagLayout());
+    private JTextField textMass;
+    private JTextField textRadius;
+    private JButton calculate;
+
+    private int mass;
+    private int radius;
 
     public GUI(String name, int width, int height) {
-        JFrame frame = new JFrame(name);
-        JPanel panel = new JPanel();
-        JTextField mass = new JTextField("Enter mass:");
+        initCalculator();
+        frame.setTitle(name);
         frame.setSize(width, height);
-        frame.add(panel);
-        panel.add(mass);
-        frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+        // Action listener
+        calculate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!textMass.getText().equals("")) {
+                    mass = Integer.parseInt(textMass.getText());
+                }
+                if (!textRadius.getText().equals("")) {
+                    radius = Integer.parseInt(textRadius.getText());
+                }
+            }
+        });
+    }
+
+    public void initCalculator() {
+        // Initializing variables
+        JLabel labelMass = new JLabel("Mass:");
+        JLabel labelRadius = new JLabel("Radius:");
+        textMass = new JTextField(5);
+        textRadius = new JTextField(5);
+        calculate = new JButton("Calculate");
+
+        // Visual
+        frame.add(panel);
+        panel.add(labelMass);
+        panel.add(textMass);
+        panel.add(labelRadius);
+        panel.add(textRadius);
+        panel.add(calculate);
+
+        frame.pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI("Physics simulator", 800, 800);
+        GUI gui = new GUI("Physics calculator", 800, 800);
         Object object = new Object(60, 0.5f);
         System.out.println(object.potentialEnergy(5) + "N");
         System.out.println(object.density() + "kg/m^3");
